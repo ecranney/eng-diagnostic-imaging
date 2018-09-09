43c9558e98911e52626b7f2b12fba6dfc41b71c9
Shalitha Weerakoon Karunatilleke <sweerakoon@student.unimelb.edu.au>
2018-09-09 11:08:25 +1000

    [Shalitha] Code retrofit

--- src/db/DBConnection.java
+++ src/db/DBConnection.java
@@ -11,32 +11,17 @@ public class DBConnection {
 	private static final String DB_USER = "user";
 	private static final String DB_PASSWORD = "password";
 
-	public static PreparedStatement prepare(String stm) throws SQLException {
-		PreparedStatement preparedStatement = null;
-		try {
-			Connection dbConnection = getDBConnection();
-			preparedStatement = dbConnection.prepareStatement(stm);
-		} catch (SQLException e) {
-			System.out.println(e.getMessage());
-		}
-		return preparedStatement;
-	}
-
-	public static Connection getDBConnection() {
+	public Connection getConnection() throws SQLException {
 		System.out.println("-------- PostgreSQL JDBC Connection Testing ------------");
-		try {
-			PGSimpleDataSource ds = new PGSimpleDataSource();
-			ds.setServerName(DB_SERVERNAME);
-			ds.setDatabaseName(DB_NAME);
-			ds.setUser(DB_USER);
-			ds.setPassword(DB_PASSWORD);
-			ds.setPortNumber(DB_SERVERPORT);
-			Connection dbConnection = ds.getConnection();
-			return dbConnection;
-		} catch (SQLException e) {
-			System.out.println(e.getMessage());
-		}
-		System.out.println("Connection problem");
-		return null;
+
+		PGSimpleDataSource ds = new PGSimpleDataSource(); 
+		ds.setServerName("localhost"); 
+		ds.setDatabaseName("dies");
+		ds.setUser("user"); 
+		ds.setPassword("password");
+		ds.setPortNumber(5432);
+		
+		Connection dbConnection = ds.getConnection();
+		return dbConnection;
 	}
 }
--- src/dies/mappers/UserMapper.java
+++ src/dies/mappers/UserMapper.java
@@ -1,43 +1,29 @@
 package dies.mappers;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.*;
 
-import javax.annotation.Resource;
-import javax.sql.DataSource;
-
-import org.postgresql.ds.PGSimpleDataSource;
-
+import db.DBConnection;
 import dies.models.*;
-import db.DBConnection;;
 
 public class UserMapper extends DataMapper {
 
-	private DataSource ds;
-
-	public User find(String username, String password) throws SQLException {
-		PGSimpleDataSource ds = new PGSimpleDataSource(); 
-		ds.setServerName("localhost"); 
-		ds.setDatabaseName("dies"); 
-		ds.setUser("user"); 
-		ds.setPassword("password");
-		Connection con = ds.getConnection();
-
-		String selectTableSQL = "SELECT username, password from public.user where username='admin' and password='admin'";
-		System.out.println(selectTableSQL);
-
-		PreparedStatement statement = con.prepareStatement(selectTableSQL);
-
+	private DBConnection db = new DBConnection();
+	private String findUserSQL = "SELECT username, password from public.user where username=? and password=?";
+	
+	public User find(String username, String password) throws SQLException {		
+		Connection con = db.getConnection();
+		PreparedStatement statement = con.prepareStatement(findUserSQL);
+		statement.setString(1, username);
+		statement.setString(2, password);
 		User user = null;
 
 		ResultSet rs = statement.executeQuery();
 		while (rs.next()) {
 			user = new User(0, rs.getString("username"), rs.getString("password"));
-			System.out.println(rs.getString("username") + " this is the username ");
 		}
 		return user;
 	}
