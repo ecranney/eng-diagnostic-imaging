53838c57492a9e32060ad013ccc422e348ec7ad4
Shalitha Weerakoon Karunatilleke <sweerakoon@student.unimelb.edu.au>
2018-09-10 23:43:55 +1000

    [Shalitha] Fixed the database

--- sql_db_setup.sql
+++ sql_db_setup.sql
@@ -1,170 +1,167 @@
-﻿--CREATE ROLE "user" LOGIN
---  ENCRYPTED PASSWORD 'md54d45974e13472b5a0be3533de4666414'
---  SUPERUSER INHERIT CREATEDB CREATEROLE REPLICATION;
+--create role "user" login
+--  encrypted password 'md54d45974e13472b5a0be3533de4666414'
+--  superuser inherit createdb createrole replication;
 --
---Database: dies
---DROP DATABASE dies;
+--database: dies
+--drop database dies;
 --
---CREATE DATABASE dies
--- WITH OWNER = user
---      ENCODING = 'UTF8'
---      TABLESPACE = pg_default
---      LC_COLLATE = 'English_Australia.1252'
---      LC_CTYPE = 'English_Australia.1252'
---      CONNECTION LIMIT = -1;
-
---drop table public.user;
---
---CREATE TABLE public.user
---(
---  id BIGSERIAL PRIMARY KEY,
---  username text NOT NULL,
---  password text NOT NULL,
---  firstname text,
---  lastname text
---);
---
---INSERT INTO public.USER(username, password, firstname, lastname)
---VALUES
--- ('admin', 'admin', 'firsname1', 'lastname1');
+--create database dies
+-- with owner = user
+--      encoding = 'utf8'
+--      tablespace = pg_default
+--      lc_collate = 'english_australia.1252'
+--      lc_ctype = 'english_australia.1252'
+--      connection limit = -1;
 
 
-SELECT
+drop table public.machine;
+drop table public.appointment_machine;
+
+drop table public.user;
+create table public.user
+(
+  id bigserial primary key,
+  username text not null,
+  password text not null,
+  firstname text,
+  lastname text
+);
+
+insert into public.user(username, password, firstname, lastname)
+values
+('admin', 'admin', 'evan', 'cranney'),
+('rob', 'rob', 'robert', 'de nero'),
+('tom', 'tom', 'tom', 'hanks');
+
+
+
+insert into public.technician(qualification)
+values
+('');
+
+drop table public.technician;
+create table public.technician(
+id		bigserial primary key,
+ qualification      text
+);
+
+select * from public.appointment;
+select * from public.user;
+
+
+
+
+select
  t1.id,
  t1.firstname,
  t1.lastname
- FROM
+ from
  public.user t1
-INNER JOIN public.technician t2 ON t1.id = t2.id;
-
-
-
-
-
-
-
-
-
-
+inner join public.technician t2 on t1.id = t2.id;
 
 
 
 select * from appointment
 
-SELECT username, password, firstname, lastname from public.user where username='admin' 
+select username, password, firstname, lastname from public.user where username='admin' 
 
 drop table public.appointment;
 
-CREATE TABLE public.appointment(
- ID	       	BIGSERIAL PRIMARY KEY,
- DATE          	TIMESTAMP,
- PATIENT_ID    	INT,
- TECHNICIAN_ID 	INT,
- APPOINTMENT_MACHINE_ID	INT,
- STATE	       	TEXT 
+create table public.appointment(
+ id	       	bigserial primary key,
+ date          	timestamp,
+ patient_id    	int,
+ technician_id 	int,
+ appointment_machine_id	int,
+ state	       	text 
 );
 
-INSERT INTO public.appointment(date, patient_id, technician_id, APPOINTMENT_MACHINE_ID, state)
-VALUES
- (current_timestamp, '1', '1', '1', 'MISSED');
+insert into public.appointment(date, patient_id, technician_id, appointment_machine_id, state)
+values
+ (current_timestamp, '1', '1', '1', 'missed');
 
- INSERT INTO public.appointment(date, patient_id, technician_id, APPOINTMENT_MACHINE_ID, state)
-VALUES
- (current_timestamp, '1', '1', '1', 'FUTURE');
+ insert into public.appointment(date, patient_id, technician_id, appointment_machine_id, state)
+values
+ (current_timestamp, '1', '1', '1', 'future');
 
---DROP TABLE public.MACHINE;
---DROP TABLE public.APPOINTMENT_MACHINE;
---
 
---INSERT INTO public.TECHNICIAN(QUALIFICATION)
---VALUES
---('');
---
---drop table public.TECHNICIAN;
---CREATE TABLE public.TECHNICIAN(
--- ID		BIGSERIAL PRIMARY KEY,
--- QUALIFICATION      TEXT
---);
---
---select * from public.appointment;
---select * from public.user;
-
-SELECT t1.id as ap_id, t1.date as ap_date, t1.state as ap_state,
-t1.patient_id as patient_id, t2.FIRST_NAME as patient_first_name, t2.last_name as patient_last_name, 
+select t1.id as ap_id, t1.date as ap_date, t1.state as ap_state,
+t1.patient_id as patient_id, t2.first_name as patient_first_name, t2.last_name as patient_last_name, 
 t6.id as patient_address_id, t6.unit_no as patient_unit_no, t6.street_no as patient_street_no, 
 t6.street_name as patient_street_name, t6.city as patient_city, t6.state as patient_state, t6.post_code as patient_post_code, 
 t2.phone as patient_mobile, 
 t2.medicare_no as patient_medicate_no,
 t5join.id as tech_id, t5join.username as tech_username, t5join.password as tech_password,
 t5join.firstname as tech_first_name, t5join.lastname as tech_last_name,
-t8join.MACHINE_id, t8join.SERIAL_CODE, t8join.TYPE
+t8join.machine_id, t8join.serial_code, t8join.type
 from public.appointment t1
 inner join public.patient t2
 on t1.patient_id = t2.id
-FULL OUTER join 
-(SELECT t4.id, t4.username, t4.password, t4.firstname, t4.lastname
+full outer join 
+(select t4.id, t4.username, t4.password, t4.firstname, t4.lastname
 from public.user t4
-inner join public.TECHNICIAN t3
+inner join public.technician t3
 on t4.id = t3.id
-) t5join on t5join.id = t1.TECHNICIAN_ID
+) t5join on t5join.id = t1.technician_id
 left join public.address t6
 on t6.id = t2.address_id
 full outer join
-(SELECT t7.id, t7.MACHINE_id, t7.APPOINTMENT_ID, t8.SERIAL_CODE, t8.TYPE
-from public.APPOINTMENT_MACHINE t7
-FULL OUTER join public.MACHINE t8
-on t7.MACHINE_id = t8.id)
-t8join on t8join.APPOINTMENT_ID = t1.ID
+(select t7.id, t7.machine_id, t7.appointment_id, t8.serial_code, t8.type
+from public.appointment_machine t7
+full outer join public.machine t8
+on t7.machine_id = t8.id)
+t8join on t8join.appointment_id = t1.id
 where t1.id = 4
 
 
-SELECT t1.id as ap_id, t1.date as ap_date, t1.state as ap_state,
-t2.FIRST_NAME as patient_first_name, t2.last_name as patient_last_name, 
+select t1.id as ap_id, t1.date as ap_date, t1.state as ap_state,
+t2.first_name as patient_first_name, t2.last_name as patient_last_name, 
 t2.medicare_no as patient_medicate_no
 from public.appointment t1
 inner join public.patient t2
 on t1.patient_id = t2.id
-INNER join 
-(SELECT t4.id, t4.firstname, t4.lastname
+inner join 
+(select t4.id, t4.firstname, t4.lastname
 from public.user t4
-inner join public.TECHNICIAN t3
+inner join public.technician t3
 on t4.id = t3.id
-) t5join on t5join.id = t1.TECHNICIAN_ID
+) t5join on t5join.id = t1.technician_id
 inner join public.address t6
 on t6.id = t2.address_id
 
 
 
 
---INSERT INTO public.APPOINTMENT_MACHINE(APPOINTMENT_ID, MACHINE_id)
---VALUES
+
+--insert into public.appointment_machine(appointment_id, machine_id)
+--values
 --('1','2');
 
-DROP Table public.machine;
-CREATE TABLE public.MACHINE(
- ID	      	BIGSERIAL PRIMARY KEY,
- SERIAL_CODE    INT,
- TYPE     	TEXT
+drop table public.machine;
+create table public.machine(
+ id	      	bigserial primary key,
+ serial_code    int,
+ type     	text
 );
 
-INSERT INTO public.MACHINE(SERIAL_CODE, TYPE)
-VALUES
- ('122334', 'CAT');
+insert into public.machine(serial_code, type)
+values
+ ('122334', 'cat');
 
-INSERT INTO public.MACHINE(SERIAL_CODE, TYPE)
-VALUES
-('999999', 'XRAY');
+insert into public.machine(serial_code, type)
+values
+('999999', 'xray');
 
---CREATE TABLE public.APPOINTMENT_MACHINE(
--- ID	       	BIGSERIAL PRIMARY KEY,
--- APPOINTMENT_ID INT,
--- MACHINE_ID 	INT
+--create table public.appointment_machine(
+-- id	       	bigserial primary key,
+-- appointment_id int,
+-- machine_id 	int
 --);
 --
---CREATE TABLE public.MACHINE(
--- ID	      	BIGSERIAL PRIMARY KEY,
--- SERIAL_CODE    TEXT,
--- TYPE     	TEXT
+--create table public.machine(
+-- id	      	bigserial primary key,
+-- serial_code    text,
+-- type     	text
 --);
 -- 
 
@@ -174,63 +171,64 @@ VALUES
 --
 
 --
-DROP TABLE public.ADDRESS;
-CREATE TABLE public.ADDRESS(
- ID	      	BIGSERIAL PRIMARY KEY,
- UNIT_NO      	INT,
- STREET_NO    	INT,
- STREET_NAME  	TEXT,
- CITY	      	TEXT,
- STATE        	TEXT,
- POST_CODE    	INT
+drop table public.address;
+create table public.address(
+ id	      	bigserial primary key,
+ unit_no      	int,
+ street_no    	int,
+ street_name  	text,
+ city	      	text,
+ state        	text,
+ post_code    	int
 );
 
-INSERT INTO public.ADDRESS(UNIT_NO, STREET_NO, STREET_NAME, CITY, STATE, POST_CODE)
-VALUES
-('0', '92', 'Station St', 'carlton', 'VIC', '3053');
+insert into public.address(unit_no, street_no, street_name, city, state, post_code)
+values
+('0', '92', 'station st', 'carlton', 'vic', '3053');
 
---drop table public.PATIENT;
---CREATE TABLE public.PATIENT(
--- ID	      	BIGSERIAL PRIMARY KEY,
--- FIRST_NAME    	TEXT,
--- LAST_NAME     	TEXT,
--- ADDRESS_ID    	INT,
--- PHONE         	TEXT,
--- MEDICARE_NO   	TEXT 
+--drop table public.patient;
+--create table public.patient(
+-- id	      	bigserial primary key,
+-- first_name    	text,
+-- last_name     	text,
+-- address_id    	int,
+-- phone         	text,
+-- medicare_no   	text 
 --);
 --
---INSERT INTO public.PATIENT(first_name, last_name, address_id, phone, medicare_no)
---VALUES ('shalitha', 'weerakoon', '1', '123456789', '1');
+--insert into public.patient(first_name, last_name, address_id, phone, medicare_no)
+--values ('shalitha', 'weerakoon', '1', '123456789', '1');
 --
 --select * from public.patient;
 
 
---CREATE TABLE public.APPOINTMENT_MACHINE(
--- ID	       	BIGSERIAL PRIMARY KEY,
--- APPOINTMENT_ID INT,
--- MACHINE_ID 	INT
+--create table public.appointment_machine(
+-- id	       	bigserial primary key,
+-- appointment_id int,
+-- machine_id 	int
 --);
 --
 
 --
 
 --
---CREATE TABLE public.RADIOLOGIST(
--- ID		INT PRIMARY KEY,
--- REG_NO 	INT
+--create table public.radiologist(
+-- id		int primary key,
+-- reg_no 	int
 --);
 --
---CREATE TABLE public.RECEPTIONIST(
--- ID		INT PRIMARY KEY
+--create table public.receptionist(
+-- id		int primary key
 --);
 --
 --
 
 --
---CREATE TABLE public.USERS(
--- ID	      	INT PRIMARY KEY,
--- FIRST_NAME    	TEXT,
--- LAST_NAME     	TEXT
+--create table public.users(
+-- id	      	int primary key,
+-- first_name    	text,
+-- last_name     	text
 --);
 --
+
 
