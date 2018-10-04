5b64d9179bb0bf33a07a80ce01e7b3484453ca51
Shalitha Weerakoon Karunatilleke <sweerakoon@student.unimelb.edu.au>
2018-10-05 03:11:34 +1000

    [Shalitha] Minor fixes

--- src/dies/controllers/LogoutServlet.java
+++ src/dies/controllers/LogoutServlet.java
@@ -6,16 +6,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.mgt.DefaultSecurityManager;
-import org.apache.shiro.session.Session;
-import org.apache.shiro.session.mgt.DefaultSessionManager;
 import org.apache.shiro.subject.Subject;
 
 import dies.auth.LoginSession;
 
 import java.io.IOException;
-import java.util.Collection;
 
 /**
  * Servlet implementation class LogoutServlet
